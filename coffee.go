0a4f727b8fbc6f69e00cad1b15b86e664df56dda
pticptenec <84808603+pticptenec@users.noreply.github.com>
2022-01-01 21:19:36 +0300

    add Device control
    water, grind, beans tanks
    +tests on lamps

--- coffee.go
+++ coffee.go
@@ -1,78 +1,190 @@
 package machine
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"strings"
+)
 
 func main() {
-
+	// TODO 1: add errors as set
+	// TODO 2: go routine loop
 }
 
-type CmdReader interface {
-	GetCmd() Command
+type Config struct {
+	Water int
+	Beans int
+	Grind int
 }
 
-type MemCommands struct {
-	commands []Command
-}
+const (
+	waterTank = "water"
+	beansTank = "beans"
+	grindTank = "grind"
+)
 
-func NewMemCommands() *MemCommands {
-	mc := MemCommands{}
-	mc.commands = make([]Command, 0)
-	return &mc
-}
-
-func (mc *MemCommands) Push(cmd Command) {
-	mc.commands = append(mc.commands, cmd)
-}
-
-func (mc *MemCommands) GetCmd() Command {
-	cmd := mc.commands[0]
-	mc.commands = mc.commands[1:]
-	return cmd
+var ErrTank = func(tank string) error {
+	return errors.New(
+		fmt.Sprintf("%s %s", strings.Title(tank),
+			"not valid state"),
+	)
 }
 
 type Machine struct {
-	devices []Device
-	maps    map[Command]func()
+	waterTank WaterTank
+	beansTank BeansTank
+	grindTank GrindTank
+	Display   RunLine
 }
 
-func NewMachine() *Machine {
-	m := &Machine{}
-	_maps := map[Command]func(){
-		OffCommand: m.Off,
+func NewMachine(c Config) *Machine {
+	lamps := [3]Lamp{{}, {}, {}}
+	wt := WaterTank{water: c.Water, lamp: &lamps[0]}
+	bt := BeansTank{beans: c.Beans, lamp: &lamps[1]}
+	gt := GrindTank{grind: c.Grind, lamp: &lamps[2]}
+
+	m := &Machine{
+		waterTank: wt,
+		beansTank: bt,
+		grindTank: gt,
 	}
-	m.maps = _maps
 	return m
 }
 
-func (m *Machine) On(input CmdReader) (exit string) {
-	defer func() {
-		if r := recover(); r.(Command) == OffCommand {
-			exit = fmt.Sprintf("Exit by %d Command", OffCommand)
-		} else {
-			panic(r)
-		}
-	}()
+type RunLine struct {
+	slug string
+}
 
-	for _, d := range m.devices {
-		d.Check()
+func (r *RunLine) Hello() {
+	r.slug = fmt.Sprintf("Coffee?\n")
+}
+
+func (r *RunLine) Ready(water, beans bool) {
+	r.slug = fmt.Sprintf("Status:\nW: %t, B: %t\n", water, beans)
+}
+
+func (r *RunLine) NotReady() {
+	return
+}
+
+type GrindTank struct {
+	lamp  *Lamp
+	grind int
+}
+
+const GrindTankMax = 100
+
+func (gt GrindTank) Check() bool {
+	if gt.grind > GrindTankMax {
+		gt.lamp.On()
+		return false
 	}
+	return true
+}
 
-	for {
-		cmd := input.GetCmd()
-		exec, ok := m.maps[cmd]
-		if ok == false {
-			panic("something wrong")
-		}
-		exec()
+func (gt GrindTank) Status() bool {
+	return gt.lamp.on
+}
+
+func (gt GrindTank) Do(grind int) error {
+	if gt.Check() == false {
+		return ErrTank(grindTank)
+	}
+	gt.grind += grind
+	gt.Check()
+	return nil
+}
+
+type BeansTank struct {
+	lamp  *Lamp
+	beans int
+}
+
+const BeansTankMin = 10
+
+func (bt BeansTank) Check() bool {
+	if bt.beans < BeansTankMin {
+		bt.lamp.On()
+		return false
+	}
+	return true
+}
+
+func (bt BeansTank) Status() bool {
+	return bt.lamp.on
+}
+
+func (bt BeansTank) Do(quantity int) error {
+	if bt.Check() == false {
+		return ErrTank(beansTank)
+	}
+	bt.beans -= quantity
+	_ = bt.Check()
+	return nil
+}
+
+type WaterTank struct {
+	lamp  *Lamp
+	water int
+}
+
+const WaterTankMin = 10
+
+func (wt WaterTank) Check() bool {
+	if wt.water < WaterTankMin {
+		wt.lamp.On()
+		return false
+	}
+	return true
+}
+
+func (wt WaterTank) Status() bool {
+	return wt.lamp.on
+}
+
+func (wt WaterTank) Do(quantity int) error {
+	if wt.Check() == false {
+		return ErrTank(waterTank)
+	}
+	wt.water -= quantity
+	_ = wt.Check()
+	return nil
+}
+
+type Lamp struct {
+	on bool
+}
+
+func (l *Lamp) On() {
+	l.on = true
+}
+
+func (l *Lamp) Off() {
+	l.on = false
+}
+
+type Device interface {
+	Check() bool
+	Do(int) error
+	Status() bool
+}
+
+func (m *Machine) On() {
+	m.Display.Hello()
+	devices := [...]Device{m.beansTank, m.waterTank, m.grindTank}
+	var ready bool = false
+	for _, d := range devices {
+		ready = d.Check()
+	}
+	if !ready {
+		m.Display.NotReady()
+	} else {
+		m.Display.Ready(m.waterTank.Status(), m.beansTank.Status())
 	}
 }
 
 func (m *Machine) Off() {
 	panic(OffCommand)
-}
-
-type Device interface {
-	Check()
 }
 
 type Command struct {
--- coffee_test.go
+++ coffee_test.go
@@ -5,14 +5,32 @@ import (
 	"testing"
 )
 
-func TestOnOffMachine(t *testing.T) {
-	m := NewMachine()
+var c Config
 
-	off := OffCommand
-	cmds := NewMemCommands()
-	cmds.Push(off)
-	exit := m.On(cmds)
-	if exit != fmt.Sprintf("Exit by %d Command", off) {
-		t.Error("exit not work")
+func TestOnMachineChecksDone(t *testing.T) {
+	c = Config{Water: 50, Beans: 30, Grind: 30}
+	m := NewMachine(c)
+	m.On()
+
+	lamps := [...]bool{m.beansTank.Status(), m.waterTank.Status(),
+		m.grindTank.Status()}
+	for _, l := range lamps {
+		if l != false {
+			t.Error("wrong Check command")
+		}
+	}
+}
+
+func TestOnMachineChecksDoneLampsOn(t *testing.T) {
+	c = Config{Water: 5, Beans: 5, Grind: 110}
+	m := NewMachine(c)
+	m.On()
+
+	lamps := [...]bool{m.beansTank.Status(), m.waterTank.Status(),
+		m.grindTank.Status()}
+	for i, l := range lamps {
+		if l != true {
+			t.Error(fmt.Sprintf("%d wrong Check command", i))
+		}
 	}
 }
